73106578b3e326d7905a01fe3dd51111cd4eb911
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2023-07-29 19:18:25 -0400

    feat: add fastify example

---
+++ src/fastify/api.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify';
+
+const server = fastify();
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+server.get('/ping', async (_request, _reply) => {
+  return 'pong\n';
+});
+
+server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
+  if (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    process.exit(1);
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(`Server listening at ${address}`);
+});
