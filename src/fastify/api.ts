69bbc97650f4bbed57ef933b6590e480c416fa20
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-06-01 16:13:03 -0400

    chore: clean up

--- src/fastify/api.ts
+++ src/fastify/api.ts
@@ -3,9 +3,7 @@ import fastify from 'fastify';
 const server = fastify();
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-server.get('/ping', async (_request, _reply) => {
-  return 'pong\n';
-});
+server.get('/ping', async (_request, _reply) => 'pong\n');
 
 server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
   if (err) {
--- src/feathers/api.ts
+++ src/feathers/api.ts
@@ -19,12 +19,12 @@ interface Message {
 class MessageService {
   messages: Message[] = [];
 
-  async find() {
+  find = async () => {
     // Just return all our messages
     return this.messages;
-  }
+  };
 
-  async create(data: Pick<Message, 'text'>) {
+  create = async (data: Pick<Message, 'text'>) => {
     // The new message is the data text with a unique identifier added
     // using the messages length since it changes whenever we add one
     const message: Message = {
@@ -36,7 +36,7 @@ class MessageService {
     this.messages.push(message);
 
     return message;
-  }
+  };
 }
 
 // This tells TypeScript what services we are registering
