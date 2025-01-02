42d9781c8ff015b86ccffd6d4c35ce43e1168332
Tramaine Gillus <github@tramaine.dev>
2025-01-01 20:54:03 -0500

    feat: add fastify swagger lambda example

---
+++ src/fastify/swagger-lambda.ts
@@ -0,0 +1,33 @@
+import awsLambdaFastify from '@fastify/aws-lambda';
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
+import fastify from 'fastify';
+
+const server = fastify();
+
+server.register(swagger, {
+  mode: 'static',
+  specification: {
+    path: './src/fastify/openapi.yaml',
+    baseDir: './src/fastify',
+  },
+});
+
+server.register(swaggerUi, {
+  routePrefix: '/docs',
+  uiConfig: {
+    docExpansion: 'list',
+    deepLinking: true,
+  },
+  theme: {
+    css: [
+      {
+        filename: 'theme.css',
+        content: '.swagger-ui .topbar { display: none !important; }',
+      },
+    ],
+  },
+  staticCSP: false,
+});
+
+export const handler = awsLambdaFastify(server);
